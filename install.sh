19beb6c4d0bf623538f9da5bf14970e39b1d57d6
Ahmad Rafi'i <70046808+rafia9005@users.noreply.github.com>
2023-12-23 19:49:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+clear
 echo "                  _                                  _                   "
 echo "   __ _   _   _  | |_    ___            ___    ___  | |_   _   _   _ __  "
 echo "  / _\` | | | | | | __|  / _ \   _____  / __|  / _ \ | __| | | | | | '_ \ "
@@ -5,8 +6,6 @@ echo " | (_| | | |_| | | |_  | (_) | |_____| \__ \ |  __/ | |_  | |_| | | |_) |"
 echo "  \__,_|  \__,_|  \__|  \___/          |___/  \___|  \__|  \__,_| | .__/ "
 echo "                                                     github.com/rafia9005"
 echo " "
-
-read -p "are you sure to install auto-setup? Y/Enter to install & CTRL + C to cancel"
 
     echo "--- wait auto setup server is running ----"
     yes "" | apt update
